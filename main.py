636cf2e0e990882dea74284f986e8c78b11a6473
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-16 14:02:43 +1000

    Added to dos in main py file

---
+++ main.py
@@ -0,0 +1,8 @@
+
+# TODO-1: Include an ASCII art logo.
+# TODO-2: Allow the player to submit a guess for a number between 1 and 100.
+# TODO-3: Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# TODO-4: If they got the answer correct, show the actual answer to the player.
+# TODO-5: Track the number of turns remaining.
+# TODO-6: If they run out of turns, provide feedback to the player. 
+# TODO-7: Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).

c233299db9515fa8a0bf109efc8ee9550e72b73e
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-16 14:49:02 +1000

    Added while loop for now, will continue later

--- main.py
+++ main.py
@@ -4,38 +4,32 @@ from random import randint
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
-# def random_number():
-#     '''A function that chooses random number 1-100'''
-#     return randint(1, 100)
-
-# answer = random_number()
-answer = randint(1, 100)
-print(f"Pssst, the correct answer is {answer}")
-
-easy = 10
-hard = 5
-attempts = 0
-
 def difficulty():
+    attempts = 0
     if input("Choose a difficulty. Type 'easy' or 'hard': ").lower() == 'easy':
         attempts = easy
     else:
         attempts = hard
     return attempts
 
-attempts = difficulty()
-
-print(f"You have {attempts} attempts remaining to guess the number.")
-
-
-# guess = int(input("Make a guess: "))
-
-
 def check_guess():
     if guess < answer:
+        attempts -= 1
         print("Too low.")
     elif guess > answer:
+        attempts -= 1
         print("Too high.")
-    # else:
-        # print(f"You got it! The answer was {answer}")
+    else:
+        print(f"You got it! The answer was {answer}")
 
+answer = randint(1, 100)
+print(f"Pssst, the correct answer is {answer}")
+
+easy = 10
+hard = 5
+attempts = difficulty()
+
+while True:
+    print(f"You have {attempts} attempts remaining to guess the number.")
+    guess = int(input("Make a guess: "))
+    check_guess()

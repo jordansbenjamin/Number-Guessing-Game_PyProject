e417353471dbd4ab7ec1d74580aea4bee6c2f3a9
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-16 14:15:46 +1000

    Added difficulty modes and prompt

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ print("I'm thinking of a number between 1 and 100.")
 answer = randint(1, 100)
 print(f"Pssst, the correct answer is {answer}")
 
+# TODO: Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+easy = 10
+hard = 5
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
 # TODO: Allow the player to submit a guess for a number between 1 and 100.
 guess = int(input("Make a guess: "))
 
@@ -22,5 +28,3 @@ guess = int(input("Make a guess: "))
 # TODO: Track the number of turns remaining.
 
 # TODO: If they run out of turns, provide feedback to the player. 
-
-# TODO: Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).

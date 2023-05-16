ce3a1ed8fc24ec1bea515c29557dcfb7bb8f2fbd
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-16 14:30:34 +1000

    Added initial check guess function + other variables

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import randint
 # TODO: Include an ASCII art logo.
-
+# print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
@@ -16,14 +16,21 @@ print(f"Pssst, the correct answer is {answer}")
 easy = 10
 hard = 5
 
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+attempts = print(f"You have {difficulty} attempts remaining to guess the number")
 
 # TODO: Allow the player to submit a guess for a number between 1 and 100.
 guess = int(input("Make a guess: "))
 
-# TODO: Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# TODO: Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
+def check_guess():
+    if guess < answer:
+        print("Too low.")
+    elif guess > answer:
+        print("Too high.")
 
 # TODO: If they got the answer correct, show the actual answer to the player.
+# print(f"You got it! The answer was {answer}")
 
 # TODO: Track the number of turns remaining.
 

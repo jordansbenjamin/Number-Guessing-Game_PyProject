e107a2da5911eaa9569946afc53a829184f7711c
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-17 13:07:19 +1000

    Added ASCII art logo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import randint
+from art import logo
 
-# print(logo)
+print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
@@ -12,15 +13,18 @@ def difficulty():
         attempts = hard
     return attempts
 
-def check_guess():
+def check_guess(attempts):
     if guess < answer:
         attempts -= 1
         print("Too low.")
+        return answer
     elif guess > answer:
         attempts -= 1
         print("Too high.")
+        return answer
     else:
         print(f"You got it! The answer was {answer}")
+        return answer
 
 answer = randint(1, 100)
 print(f"Pssst, the correct answer is {answer}")
@@ -32,4 +36,9 @@ attempts = difficulty()
 while True:
     print(f"You have {attempts} attempts remaining to guess the number.")
     guess = int(input("Make a guess: "))
-    check_guess()
+    check_guess = check_guess(attempts)
+    if check_guess != answer:
+        continue
+    else:
+        break
+    
